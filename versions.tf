4b1c73ee6583d9b16ad011def6fde1fe06f8f78d
Dan Gibbons <dan.gibbons@wiz.io>
2022-09-20 11:08:45 +0100

    Inrease AWS version

--- versions.tf
+++ versions.tf
@@ -1,11 +1,10 @@
 terraform {
   required_version = ">= 1.0"
 
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4"
+      version = ">= 4.9"
     }
   }
 }
